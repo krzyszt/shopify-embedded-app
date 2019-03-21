f006ca5bf5a197829fe77367371037e6d55d64d1
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2019-03-21 08:45:10 +0000

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import '@shopify/polaris/styles.css';
+import Cookies from 'js-cookie'
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  fetchOptions: {
+    credentials: 'include'
+  }  
+});
+
+class MyApp extends App {
+  constructor() {
+    super();
+    this.state = { 
+      shopOrigin: Cookies.get('shopOrigin') 
+    };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+          <AppProvider shopOrigin={this.state.shopOrigin} apiKey={API_KEY}>
+            <ApolloProvider client={client}>
+              <Component {...pageProps} />
+            </ApolloProvider>
+          </AppProvider>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyApp;
